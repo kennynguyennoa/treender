577f9688c3e45e2064337ec489a2b11cfb099af0
Nicolai Rønning <97887844+nicolainoaignite@users.noreply.github.com>
2022-03-25 18:00:25 +0100

    Init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className='App'>
-      <h1>Treender</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="swiper" element={<Swiper />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="message" element={<Message />} />
+      </Routes>
     </div>
   );
 }
